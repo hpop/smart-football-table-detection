9e5a9428044b7a2723a6b252e2825f91b8066260
Helmut Poppen <helmut@coffeecoder.de>
2020-06-27 18:52:42 +0200

    test

--- opencv_yolo/darknet_video.py
+++ opencv_yolo/darknet_video.py
@@ -168,9 +168,7 @@ def YOLO():
                                    interpolation=cv2.INTER_LINEAR)
 
         darknet.copy_image_from_bytes(darknet_image,frame_resized.tobytes())
-        print("detect")
         detections = darknet.detect_image(netMain, metaMain, darknet_image, thresh=0.25)
-        print("detect done")
         position = (-1,-1)
 
 
